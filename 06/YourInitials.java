bff7811f674930708bf1d0e24233769db930b801
TNjoroge <36413738+TNjoroge@users.noreply.github.com>
2018-02-16 11:03:11 -0500

    Add files via upload

---
+++ 06/YourInitials.java
@@ -0,0 +1,17 @@
+public class YourInitials
+{
+	  public static void main(String [] args) {
+
+	System.out.println("For the the initals, Ted Gitomeh Njoroge");
+	
+	System.out.println("TTTTT    GGG    N    N");
+	System.out.println("  T     G   G   NN   N");
+	System.out.println("  T     G       N  N N");
+	System.out.println("  T     GGGGG   N   NN");
+	System.out.println("  T     G   G   N    N");
+	System.out.println("  T     G   G   N    N");
+	System.out.println("  T  .   GGG  . N    N");
+	
+	}
+}
+ 
